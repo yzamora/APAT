dcab6da55efd7cd997202927a704cb6e119a455a
jmperez <jmperez@asesoftware.com>
2018-10-08 14:05:06 -0500

    Creación de la aplicación

---
+++ APP/app/src/main/java/com/asesoftware/apat/Launcher.java
@@ -0,0 +1,13 @@
+package com.asesoftware.apat;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Launcher extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_launcher);
+    }
+}
